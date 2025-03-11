5f48823d3f0b754cb431d6d7d540ff89005329a2
Kyle Huynh <32345248+kylevh@users.noreply.github.com>
2025-03-10 23:30:54 -0700

    boilerplating

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,15 @@
 import { AnimatePresence } from 'framer-motion'
 import { BrowserRouter as Router, Routes, Route } from 'react-router'
-import Home from './pages/Home'
-import Contact from './pages/Contact'
-import Projects from './pages/Projects'
-import Project from './pages/Project'
+import Home from '../pages/Home'
+import Contact from '../pages/Contact'
+import Projects from '../pages/Projects'
+import Project from '../pages/Project'
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-neutral-100 dark:bg-gray-900">
-        <main className="container mx-auto px-4 py-8">
-          <div className="flex flex-row items-center bg-red-500 justify-center">
-            <h1>Test</h1>
-          </div>
+      <div className="min-h-screen w-full flex flex-col overflow-x-hidden bg-neutral-100 ">
+        <main className="flex-1 h-full w-full">
           <AnimatePresence mode="wait">
             <Routes>
               <Route path="/" element={<Home />} />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
 const Home = () => {
     return (
-      <div className="text-center">
+      <section className="min-h-screen w-full p-4 md:p-8 flex items-center justify-center">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 max-w-6xl">
+        {/* About Card */}
+        <div className="bg-white backdrop-blur-sm rounded-lg p-6 hover:transform hover:scale-105 transition-transform">
+          <h2 className="text-2xl font-bold text-neutral-950 mb-4">About Me</h2>
+          <p className="text-neutral-500">Your introduction here...</p>
+        </div>
+
       </div>
+    </section>
     )
   }
   
