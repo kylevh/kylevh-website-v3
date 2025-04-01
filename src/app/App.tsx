d921d9b8bf5511ed875192209bfc8891c85ad8ba
Kyle Huynh <32345248+kylevh@users.noreply.github.com>
2025-04-01 10:02:01 -0700

    mobile view scroll fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,34 +5,14 @@ import Contact from "../pages/Contact";
 import Projects from "../pages/Project/Projects";
 import Project from "../pages/Project/Project";
 import Navbar from "../components/Navbar/Navbar";
-import Lenis from '@studio-freight/lenis'
-import { useEffect } from "react";
 import Blog from "../pages/Blog/Blog";
 import BlogPost from "../pages/Blog/BlogPost";
 
 function App() {
-  useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      touchMultiplier: 2,
-    });
-
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-
-    return () => {
-      lenis.destroy();
-    };
-  }, []);
   return (
     <Router>
-      <div className="min-h-screen w-full flex flex-col overflow-x-hidden bg-neutral-100">
-        <main className="flex-1 flex flex-col h-full w-full items-center mt-16">
+      <div className="h-screen w-full flex flex-col overflow-x-hidden bg-neutral-100">
+        <main className="flex-1 flex flex-col h-full w-full items-center mt-16 overflow-hidden">
         <Navbar />
           <AnimatePresence mode="wait">
             <Routes>
