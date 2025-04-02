8f1804a0ce4cedb1ed8b69d051f8b17a71f42e08
Kyle Huynh <32345248+kylevh@users.noreply.github.com>
2025-04-02 11:42:19 -0700

    m2

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <Router>
       <div className="h-screen w-full flex flex-col overflow-x-hidden bg-green-500 overflow-y-auto bg-neutral-100">
-        <main className="flex-1 flex flex-col h-auto bg-blue-500  w-full items-center ">
+        <main className="flex-1 flex flex-col h-auto bg-blue-500 w-full items-center ">
         <Navbar />
           <AnimatePresence mode="wait">
             <Routes>
